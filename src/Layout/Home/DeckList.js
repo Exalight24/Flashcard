2a505606b0c2654c6ce4068147629def3d88238f
brandon neer <btnphotography@gmail.com>
2021-03-08 17:04:58 -0600

    commit 3

--- src/Layout/Home/DeckList.js
+++ src/Layout/Home/DeckList.js
@@ -1,50 +1,50 @@
-import React from 'react'
-import { PropTypes } from 'prop-types'
-import { Link, useHistory } from 'react-router-dom'
-import { deleteDeck } from '../../utils/api/index'
+import React from "react";
+import { PropTypes } from "prop-types";
+import { Link, useHistory } from "react-router-dom";
+import { deleteDeck } from "../../utils/api/index";
 
 function DeckList({ id, name, description, cards, updateDecks }) {
-  const history = useHistory()
+  const history = useHistory();
 
   const handleDelete = async () => {
     const toDelete = window.confirm(
-      'Delete this deck? \n \n \n You will not be able to recover it.'
-    )
+      "Delete this deck? \n \n \n You will not be able to recover it."
+    );
     if (toDelete) {
-      await deleteDeck(id)
-      updateDecks(-1) // to re-render decklist on home page
-      history.push('/')
+      await deleteDeck(id);
+      updateDecks(-1); // to re-render decklist on home page
+      history.push("/");
     }
-  }
+  };
   return (
-    <div className='card' style={{ width: '40rem' }}>
-      <div className='card-body'>
-        <div className='d-flex justify-content-between'>
-          <h5 className='card-title'>{name}</h5>
+    <div className="card" style={{ width: "40rem" }}>
+      <div className="card-body">
+        <div className="d-flex justify-content-between">
+          <h5 className="card-title">{name}</h5>
           <p>{cards.length} cards</p>
         </div>
-        <p className='card-text'>{description}</p>
-        <div className='d-flex justify-content-between'>
+        <p className="card-text">{description}</p>
+        <div className="d-flex justify-content-between">
           <div>
             <Link to={`/decks/${id}`}>
-              <button className='btn btn-secondary m-1'>
-                <i className='bi bi-eye-fill'></i> View
+              <button className="btn btn-secondary m-1">
+                <i className="bi bi-eye-fill"></i> View
               </button>
             </Link>
             <Link to={`/decks/${id}/study`}>
-              <button className='btn btn-primary'>
-                <i className='bi bi-journal-bookmark'></i> Study
+              <button className="btn btn-primary">
+                <i className="bi bi-journal-bookmark"></i> Study
               </button>
             </Link>
           </div>
-          <button className='btn btn-danger' onClick={handleDelete}>
-            {' '}
-            <i className='bi bi-trash'> </i>
+          <button className="btn btn-danger" onClick={handleDelete}>
+            {" "}
+            <i className="bi bi-trash"> </i>
           </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 DeckList.propTypes = {
@@ -53,6 +53,6 @@ DeckList.propTypes = {
   description: PropTypes.string,
   cards: PropTypes.array,
   updateDecks: PropTypes.func,
-}
+};
 
-export default DeckList
+export default DeckList;
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,56 +1,161 @@
-import React, { useState } from 'react'
-import Header from './Common/Header'
-import NotFound from './Common/NotFound'
-import Home from './Home/Home'
-import CreateDeck from './Home/CreateDeck'
-import Study from './Deck/Study'
-import Deck from './Deck/Deck'
-import { Switch, Route } from 'react-router-dom'
-import EditDeck from './Deck/EditDeck'
-import AddCard from './Cards/AddCard'
-import EditCard from './Cards/EditCard'
+import { Switch, Route, useRouteMatch, useHistory } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import Header from './Header'
+import NotFound from './NotFound'
+import Decks from '../decks/Decks'
+import Deck from '../decks/Deck'
+import NewDeck from '../decks/NewDeck'
+import StudyDeck from '../decks/StudyDeck'
+import EditDeck from '../decks/EditDeck'
+import AddCard from '../cards/AddCard'
+import EditCard from '../cards/EditCard'
+import {
+  listDecks,
+  readDeck,
+  deleteDeck,
+  deleteCard,
+  listCards,
+  readCard,
+} from '../utils/api/index'
 
-function Layout() {
-  const [numOfDecks, setNumOfDecks] = useState(0)
-  const updateDecks = val => {
-    //used to re-render decklist on home page properly
-    setNumOfDecks(() => numOfDecks + val)
+export default function Layout() {
+  const [deckCollection, setDeckCollection] = useState([])
+  const [currentDeck, setCurrentDeck] = useState([])
+  const [cardCollection, setCardCollection] = useState([])
+  const [currentCard, setCurrentCard] = useState([])
+
+  const history = useHistory()
+  const url = useRouteMatch()
+  const deckId = url.params.deckId
+  const cardId = url.params.cardId
+
+  function refreshPage() {
+    window.location.reload(true)
   }
 
+  async function renderData() {
+    const setDecks = await listDecks()
+    await setDeckCollection(setDecks)
+    const setDeck = await readDeck(deckId)
+    await setCurrentDeck(setDeck)
+    const setCards = await listCards(deckId)
+    await setCardCollection(setCards)
+    if (cardId) {
+      const setCard = await readCard(cardId)
+      await setCurrentCard(setCard)
+    }
+  }
+
+  // Displays the collection of decks saved in the database
+  useEffect(() => {
+    async function displayDecks() {
+      const response = await listDecks()
+      setDeckCollection(response)
+    }
+    displayDecks()
+  }, [])
+
+  // Reads the selected deck's contents
+  function focusDeckHandler() {
+    async function findDeck() {
+      const response = await readDeck(deckId)
+      await setCurrentDeck(response)
+    }
+    findDeck()
+  }
+
+  function checkButton(event) {
+    console.log('You clicked a button with an id of:', event)
+  }
+
+  function deleteCardHandler(card) {
+    const id = card.id
+    deleteCard(id)
+    history.go(-2)
+    refreshPage()
+  }
+
+  async function deleteDeckHandler(deck) {
+    deleteDeck(deck.id)
+    history.push('/')
+    refreshPage()
+  }
   return (
     <>
       <Header />
-      <div className='container'>
+      <div className='container mb-2'>
         <Switch>
-          <Route path='/' exact={true}>
-            <Home numOfDecks={numOfDecks} updateDecks={updateDecks} />
-          </Route>
-          <Route path='/decks/:deckId/study'>
-            <Study />
+          <Route exact path='/'>
+            <Decks
+              deckCollection={deckCollection}
+              setCurrentDeck={setCurrentDeck}
+              deleteDeckHandler={deleteDeckHandler}
+              cardCollection={cardCollection}
+            />
           </Route>
           <Route path='/decks/new'>
-            <CreateDeck />
+            <NewDeck
+              deckCollection={deckCollection}
+              setDeckCollection={setDeckCollection}
+            />
           </Route>
-          <Route path='/decks/:deckId/edit'>
-            <EditDeck />
+          <Route path='/decks/:deckId/study'>
+            <StudyDeck
+              currentDeck={currentDeck}
+              setCurrentDeck={setCurrentDeck}
+              setDeckCollection={setDeckCollection}
+              setCardCollection={setCardCollection}
+            />
           </Route>
-          <Route path='/decks/:deckId' exact={true}>
-            <Deck updateDecks={updateDecks} />
+          <Route exact path='/decks/:deckId'>
+            <Deck
+              renderData={renderData}
+              currentDeck={currentDeck}
+              setCurrentDeck={setCurrentDeck}
+              setDeckCollection={setDeckCollection}
+              setCurrentCard={setCurrentCard}
+              cardCollection={cardCollection}
+              setCardCollection={setCardCollection}
+              deleteCardHandler={deleteCardHandler}
+              deleteDeckHandler={deleteDeckHandler}
+            />
           </Route>
-          <Route path='/decks/:deckId/cards/:cardId/edit'>
-            <EditCard />
+          <Route exact path='/decks/:deckId/edit'>
+            <EditDeck
+              currentDeck={currentDeck}
+              setCurrentDeck={setCurrentDeck}
+              setDeckCollection={setDeckCollection}
+              setCardCollection={setCardCollection}
+              refreshPage={refreshPage}
+            />
           </Route>
-          <Route path='/decks/:deckId/cards/new'>
-            <AddCard />
+          <Route exact path='/decks/:deckId/cards/new'>
+            <AddCard
+              currentDeck={currentDeck}
+              setCurrentDeck={setCurrentDeck}
+              cardCollection={cardCollection}
+              setCardCollection={setCardCollection}
+              setDeckCollection={setDeckCollection}
+              refreshPage={refreshPage}
+            />
+          </Route>
+          <Route exact path='/decks/:deckId/cards/:cardId/edit'>
+            <EditCard
+              currentCard={currentCard}
+              setCurrentCard={setCurrentCard}
+              currentDeck={currentDeck}
+              setCurrentDeck={setCurrentDeck}
+              setDeckCollection={setDeckCollection}
+              setCardCollection={setCardCollection}
+              refreshPage={refreshPage}
+            />
           </Route>
           <Route>
             <NotFound />
           </Route>
         </Switch>
-        <div className='container'></div>
       </div>
     </>
   )
 }
 
-export default Layout
